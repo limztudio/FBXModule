5b55b81422b3889482ed971bb416e907c2ca66dc
LimTaewoo <limstudio@rhaon.co.kr>
2020-05-26 15:53:24 +0900

    some utility functions are added

--- include/FBXUtilites.hpp
+++ include/FBXUtilites.hpp
@@ -27,11 +27,29 @@ namespace FBXUtilites{
         }
     }
     template<typename NODE, typename FUNC>
+    static void BreakableIterateAnimation(NODE* p, FUNC func){
+        if(p){
+            BreakableIterateAnimation(p->Next, func);
+            if(!func(p))
+                return;
+        }
+    }
+
+    template<typename NODE, typename FUNC>
     static void IterateNode(NODE* p, FUNC func){
         if(p){
             IterateNode(p->Child, func);
             IterateNode(p->Sibling, func);
             func(p);
+        }
+    }
+    template<typename NODE, typename FUNC>
+    static void BreakableIterateNode(NODE* p, FUNC func){
+        if(p){
+            BreakableIterateNode(p->Child, func);
+            BreakableIterateNode(p->Sibling, func);
+            if(!func(p))
+                return;
         }
     }
 };
