c09aa4f1f84b37886fdb667e7f7e1518b407216b
LimTaewoo <limstudio@rhaon.co.kr>
2020-06-01 15:45:22 +0900

    bug fixed

--- FBXModule/FBXShared_Bone.cpp
+++ FBXModule/FBXShared_Bone.cpp
@@ -18,18 +18,20 @@ using namespace fbxsdk;
 
 bool SHRLoadBoneNode(FbxManager* kSDKManager, const FbxNode* kNode, FBXBone* pNode){
     auto* kBone = (FbxSkeleton*)kNode->GetNodeAttribute();
-
-    pNode->Size = decltype(pNode->Size)(kBone->Size);
-    pNode->Length = decltype(pNode->Length)(kBone->LimbLength);
+    if(kBone){
+        pNode->Size = decltype(pNode->Size)(kBone->Size);
+        pNode->Length = decltype(pNode->Length)(kBone->LimbLength);
+    }
 
     return true;
 }
 
 bool SHRInitBoneNode(FbxManager* kSDKManager, const FBXBone* pNode, FbxNode* kNode){
-    auto* kSkeleton = kNode->GetSkeleton();
-
-    kSkeleton->Size = pNode->Size;
-    kSkeleton->LimbLength = pNode->Length;
+    auto* kBone = (FbxSkeleton*)kNode->GetNodeAttribute();
+    if(kBone){
+        kBone->Size = pNode->Size;
+        kBone->LimbLength = pNode->Length;
+    }
 
     return true;
 }
